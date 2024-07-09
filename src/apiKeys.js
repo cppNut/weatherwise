1648aaab17c873c8e99f2b13e860600813a6dd99
cppNut <mj9425326498@gmail.com>
2024-07-09 18:42:40 +0530

    initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "44cc264d0f77ccff77c4531c75b8ab2c",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
